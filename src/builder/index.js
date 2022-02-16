b3b9e5dbb2f75150821ed38d7364d2f064e28f3c
yaser2us <yaser2us@gmail.com>
2022-02-16 18:27:11 +0800

    new version added ;)

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,4 +1,5 @@
 import FormBuilderV1 from "./FormBuilderV1";
 import FormBuilderV2 from "./FormBuilderV2";
 
-export { FormBuilderV2 as DynoBuilder };
+export { FormBuilderV1 as DynoBuilder,
+    FormBuilderV2 };
