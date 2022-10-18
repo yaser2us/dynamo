071ecb6edab231c59bf6e0ea1543e68b6927f096
yaser2us <yaser2us@gmail.com>
2022-10-18 21:58:21 +0800

    action schema setup encapsulated thanks to Edward ;)

---
+++ src/middleware/index.js
@@ -0,0 +1,11 @@
+import { dataTransformer } from "./dataTransformer";
+import schemaTransformer from "./schemaTransformer";
+import schemaProxy from "./schemaProxy";
+import setupProxy from "./setupProxy";
+
+export {
+    schemaTransformer,
+    dataTransformer,
+    schemaProxy,
+    setupProxy,
+}
